56d6d41ea03574b1f666fba6eb83f2d79ae13094
jeanemmery <jean.emmery@hotmail.fr>
2022-05-13 15:07:49 +0200

    extract to multiple function

--- app/js/roman.js
+++ app/js/roman.js
@@ -1,28 +1,37 @@
 const NUMERAL_ONE = 'I';
-const FIRST_TRESHOLD = 5;
-const SECOND_TRESHOLD = 10;
+
+const FIVE_CONFIG = {
+  treshold: 5,
+  character: 'V',
+}
+
+const TEN_CONFIG = {
+  treshold: 10,
+  character: 'X',
+}
+
+export function handleTresholdCase({number, config}) {
+  const {treshold, character} = config;
+  let roman = '';
+
+  if ( number === treshold - 1 ) {
+    roman += NUMERAL_ONE;
+  }
+  roman += character;
+
+  if ( number > treshold) {
+    roman += numberToRoman(number - treshold);
+  }
+  return roman;
+}
 
 export const numberToRoman = function (number) {
   let roman = '';
 
-  if (number >= SECOND_TRESHOLD - 1) {
-    if ( number === SECOND_TRESHOLD - 1 ) {
-      roman += NUMERAL_ONE;
-    }
-    roman += 'X';
-
-    if ( number > SECOND_TRESHOLD) {
-      roman += numberToRoman(number - SECOND_TRESHOLD);
-    }
-  } else if (number >= FIRST_TRESHOLD - 1) {
-    if ( number === FIRST_TRESHOLD - 1 ) {
-      roman += NUMERAL_ONE;
-    }
-    roman += 'V';
-
-    if ( number > FIRST_TRESHOLD ) {
-      roman += numberToRoman(number - FIRST_TRESHOLD);
-    }
+  if (number >= TEN_CONFIG.treshold - 1) {
+      roman += handleTresholdCase({number, config: TEN_CONFIG});
+  } else if (number >= FIVE_CONFIG.treshold - 1) {
+      roman += handleTresholdCase({number, config: FIVE_CONFIG});
   } else  {
     for (let i = 0; i < number; i++) {
       roman += NUMERAL_ONE;
