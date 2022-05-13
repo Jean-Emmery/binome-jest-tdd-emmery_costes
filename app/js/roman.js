559aabbcc83fce68fc58b78b67b55d47a03b0353
jeanemmery <jean.emmery@hotmail.fr>
2022-05-13 15:43:21 +0200

    handle 50 case

--- app/js/roman.js
+++ app/js/roman.js
@@ -1,43 +1,60 @@
 const NUMERAL_ONE = 'I';
+const FIFTY_CONFIG = {
+    threshold: 50,
+    character: 'L',
+    stepDownAmount: 10,
+    stepDownCharacter: 'X'
+}
 
 const ROMAN_CONFIG = [
     {
-        treshold: 10,
+        threshold: 10,
         character: 'X',
+        stepDownAmount: 1,
+        stepDownCharacter: 'I'
     },
     {
-        treshold: 5,
+        threshold: 5,
         character: 'V',
+        stepDownAmount: 1,
+        stepDownCharacter: 'I'
     }
 ];
 
 export function handleTresholdCase({number, config}) {
-    const {treshold, character} = config;
+    const {threshold, character, stepDownAmount, stepDownCharacter} = config;
 
     let roman = '';
 
-    if (number === treshold - 1) {
-        roman += NUMERAL_ONE;
+    if (number === threshold - stepDownAmount) {
+        roman += stepDownCharacter;
     }
     roman += character;
 
-    if (number > treshold) {
-        roman += numberToRoman(number - treshold);
+    if (number > threshold) {
+        roman += numberToRoman(number - threshold);
     }
     return roman;
 }
 
 export const numberToRoman = function (number) {
-    if (number === 50) {
-        return 'L';
-    } else if (number >= 40 ){
-        let roman = 'XL';
-        roman += numberToRoman(number - 40);
+    const {threshold, character, stepDownAmount, stepDownCharacter} = FIFTY_CONFIG;
+
+    if (number >= threshold - stepDownAmount) {
+        let roman = '';
+        if (number !== threshold) {
+            roman += stepDownCharacter;
+        }
+        roman += character;
+        if(number !== threshold) {
+            roman += numberToRoman(number - (threshold - stepDownAmount));
+        }
         return roman;
     }
+
     for (let i = 0; i < ROMAN_CONFIG.length; i++) {
         const config = ROMAN_CONFIG[i];
-        if (number >= config.treshold - 1) {
+        if (number >= config.threshold - 1) {
             return handleTresholdCase({number, config});
         }
     }
